3efa698da91e92e1057da9f25e7a8d3e22e5cf53
vinidiias <vininoites8350@gmail.com>
2025-03-23 14:38:07 -0300

    feat: redirect to link on Footer

--- frontend/src/layouts/footer/index.jsx
+++ frontend/src/layouts/footer/index.jsx
@@ -1,18 +1,34 @@
 import FacebookIcon from '@mui/icons-material/Facebook';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import InstagramIcon from '@mui/icons-material/Instagram';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <footer className="bg-gray-800 text-white py-2 flex  items-center justify-center gap-5 z-1500">
-            <p>&copy; 2025. All right reserved.</p>
-            <ul className="flex items-center gap-2">
-                <li className="text-white cursor-pointer  hover:scale-110  transition-all  duration-500"><FacebookIcon  /></li>
-                <li className="text-white cursor-pointer  hover:scale-110  transition-all  duration-500"><LinkedInIcon  /></li>
-                <li className="text-white cursor-pointer  hover:scale-110  transition-all  duration-500"><InstagramIcon  /></li>
-            </ul>
-        </footer>
-    )
+      <footer className="bg-gray-800 text-white py-2 flex  items-center justify-center gap-5 z-1500">
+        <p>&copy; 2025. All right reserved.</p>
+        <ul className="flex items-center gap-2">
+          <li className="text-white cursor-pointer  hover:scale-110  transition-all  duration-500">
+            <Link to='https://www.facebook.com/vinicius.dias.9216778?locale=pt_BR' target='_blank'>
+                <FacebookIcon />
+            </Link>
+          </li>
+          <li className="text-white cursor-pointer  hover:scale-110  transition-all  duration-500">
+            <Link to='https://www.linkedin.com/in/vinicius-diass/' target='_blank'>
+              <LinkedInIcon />
+            </Link>
+          </li>
+          <li className="text-white cursor-pointer  hover:scale-110  transition-all  duration-500">
+            <Link
+              to="https://www.instagram.com/viniciusdiias_/"
+              target="_blank"
+            >
+              <InstagramIcon />
+            </Link>
+          </li>
+        </ul>
+      </footer>
+    );
 }
 
 export default Footer
