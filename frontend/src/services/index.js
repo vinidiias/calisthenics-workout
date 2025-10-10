afe27bb681641b590869e7948859f3c4aebbb310
Vinícius de Oliveira Dias <vinicius.dias@transp.net>
2025-10-10 17:09:53 -0300

    ajuste url api

--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -4,7 +4,7 @@ import axios from 'axios'
     //'https://calisthenics-workout.vercel.app'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3001'
+    baseURL: 'https://calisthenics-workout.vercel.app'
 })
 
 export default api
